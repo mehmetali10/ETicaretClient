f6e1f52dfc58200274c2348444e1726b9fadae88
mehmetali10 <mhmtali2316@gmail.com>
2022-09-05 13:44:05 +0300

    lesson 11 multiple layout structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 import { AdminModule } from './admin/admin.module';
 
 import { AppRoutingModule } from './app-routing.module';
